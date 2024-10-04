6453cf80aa5d4a68455637fb856984a5133c007d
RickyJary <ricardogarciajarillo0209@gmail.com>
2024-10-04 23:27:14 +0200

    centiSeconds finished!

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;      
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 100) % 60;
   }
-
+  
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 100) ;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    value = value.toString();
+    if (value.length < 2){
+      return "0" + value;
+    }
+    return value;
   }
 
   stop() {
-    // ... your code goes here
+    if(this.intervalId){
+      clearInterval(this.intervalId);
+    }
+    return clearInterval()
+    
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let mm = this.computeTwoDigitNumber(this.getMinutes());
+    let ss = this.computeTwoDigitNumber(this.getSeconds());
+    let cs = this.computeTwoDigitNumber(this.getCentiseconds())
+    return `${mm}:${ss}.${cs}`
   }
 }
